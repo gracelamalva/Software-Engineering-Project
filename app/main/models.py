821df4739f5f016be762514d29e86d94902a867c
skhan667 <sabrina.khan17@stjohns.edu>
2019-10-20 19:04:45 -0400

    Small update

--- app/main/models.py
+++ app/main/models.py
@@ -1,40 +1,40 @@
 from flask_sqlalchemy import SQLAlchemy
+from app import db
 db = SQLAlchemy()
 
 class Users(db.Model):
     __tablename__ = "Users"
-    Username = db.Column(db.Integer, primary_key=True, nullable = False)
-    fullName = db.Column(db.String, nullable = False)
-    passwordHash = db.Column(db.String, nullable = False)
-    Email = db.Column(db.String, nullable = False)
+    Username = db.Column(db.Integer, primary_key=True, nullable=False)
+    fullName = db.Column(db.String, nullable=False)
+    passwordHash = db.Column(db.String, nullable=False)
+    Email = db.Column(db.String, nullable=False)
 
     journal = db.relationship("Journal", uselist=False, back_populates="Users")
 
-
     def add_journal(self, title):
-        new_journal = Journal(title = title, J_ID = self.Username)
+        new_journal = Journal(title=title, J_ID=self.Username)
         db.session.add(new_journal)
         db.session.commit()
-    
+
 
 class Journal(db.Model):
     __tablename__ = "Journal"
-    JournalID = db.Column(db.Integer, primary_key=True, nullable = False) 
-    title = db.Column(db.String, nullable = False)
-    UserID = db.Column(db.String, db.ForeignKey('Users.Username'), nullable = False)
-    
-    entries = db.relationship("JournalEntry", back_populates = "Journal")
+    JournalID = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    UserID = db.Column(db.String, db.ForeignKey('Users.Username'), nullable=False)
+
+    entries = db.relationship("JournalEntry", back_populates="Journal")
 
     def add_entry(self, entrytitle, entrytext, date_time):
-        new_entry = JournalEntry(Entrytitle = entrytitle, EntryText = entrytext, Date_Time = date_time, J_ID = self.JournalID)
+        new_entry = JournalEntry(Entrytitle=entrytitle, EntryText=entrytext, Date_Time=date_time, J_ID=self.JournalID)
         db.session.add(new_entry)
         db.session.commit()
 
+
 class JournalEntry(db.Model):
     __tablename__ = "JournalEntry"
-    EntryID = db.Column(db.Integer, primary_key=True, nullable = False)
+    EntryID = db.Column(db.Integer, primary_key=True, nullable=False)
     EntryTitle = db.Column(db.String)
     EntryText = db.Column(db.String)
     Date_Time = db.Column(db.DateTime)
-    J_ID = db.Column(db.Integer, db.ForeignKey('Journal.JournalID'), nullable = False)
-
+    J_ID = db.Column(db.Integer, db.ForeignKey('Journal.JournalID'), nullable=False)
