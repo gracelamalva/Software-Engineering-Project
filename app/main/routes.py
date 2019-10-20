b4d921013aa4bdd469e789ae2a7ffc800d142ee2
skhan667 <sabrina.khan17@stjohns.edu>
2019-10-20 19:05:07 -0400

    Changed create journal

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,13 +2,19 @@ import sys
 from app.main import bp
 from flask import Flask, redirect, render_template, request, Blueprint, url_for, jsonify
 from app.main.models import *
+from .models import Users
+from .models import Journal
+from .models import JournalEntry
 from flask_sqlalchemy import SQLAlchemy
 
-#app = Flask(__name__)
-#bp = Blueprint("site", __name__)
+from config import Config
+from sqlalchemy.testing import db
 
+app = Flask(__name__)
+app.config.from_object(Config)
+bp = Blueprint("site", __name__)
 
-#db.init_app(app)
+db.init_app(app)
 
 @bp.route('/', methods=['GET','POST'])
 def index():
@@ -29,29 +35,27 @@ def journal():
    # if request.method == 'GET':
     #  return redirect(url_for('main.view'))
 
-    return render_template('journal.html', entries = entries)
+    return render_template('journal.html', entries=entries)
 
 @bp.route('/createjournal', methods = ['POST', 'GET'])
-def createjournal():
-
+def create_journal():
     title = request.form.get("title")
-
-    journal = Journal(title = title)
+    username = request.form.get("username")
+    journal = Journal(title=title, username=username)
     db.session.add(journal)
     db.session.commit()
     # Query database.
-
     journal=Journal.query.all()
-    return render_template('createjournal.html',journal = journal)
-
+    return render_template('createjournal.html', journal=journal)
 
 @bp.route('/edit', methods = ['GET', 'POST'])
-def edit():
+def edit(EntryID):
     entry = ['entry']
 
     if request == 'POST':
         result = request.form.get("entry")
         #update db
+        #looks for Journal Entry (using user login and journal id) deletes old entry and replaces with new one
     
     return render_template('edit.html', entry = entry)
 
@@ -65,7 +69,7 @@ def add(JournalID):
         entrytitle = request.form.get("entrytitle")
         entrytext = request.form.get("entrytext")
         datetime = request.form.get("datetime")
-        journal.add_entry(entrytitle,entrytext,datetime)
+        journal.add_entry(entrytitle, entrytext, datetime)
         # Equivalent to:
         # INSERT INTO flights (flight_number, origin, destination, durations) VALUES (origin,...)
         entry = journal.entries
@@ -73,9 +77,11 @@ def add(JournalID):
         db.session.add(entry)
         db.session.commit()
         # Query database.
+        entry = JournalEntry.query.all()
 
-        return render_template('view.html', journal = journal, entry = entry)
-    return render_template('add.html', journal = journal, entry = entry)
+
+        return render_template('view.html', journal=journal, entry=entry)
+    return render_template('add.html', journal=journal, entry=entry)
 
 @bp.route('/view/<int:JournalID>', methods = ['POST','GET'])
 def view():
