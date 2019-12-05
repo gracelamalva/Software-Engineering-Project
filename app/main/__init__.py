aa22f3ed750bfe567d9e321483e73bebdf709e69
gracelamalva <gracevlamalva@gmail.com>
2019-12-05 13:00:42 -0500

    update

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint, Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_mail import Mail
 
 from app import api
 from flask import Blueprint
@@ -12,6 +13,7 @@ bp = Blueprint('main',__name__, template_folder= '../main/templates')
 db = SQLAlchemy()
 login = LoginManager()
 flask_bcrypt = Bcrypt()
+mail = Mail()
 
 bp = Blueprint('main', __name__, template_folder='templates')
 
@@ -27,9 +29,11 @@ from app.main import config
 
 def create_app():
     app = Flask(__name__)
+    
     # db.init_app(app)
 
     app.config.from_object(config)
+    
     db.init_app(app)
 
     #
--- app/main/config.py
+++ app/main/config.py
@@ -8,3 +8,16 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
     DEBUG = True
+    
+    
+    DEBUG=True
+    #EMAIL SETTINGS
+    MAIL_SERVER='smtp.gmail.com'
+    MAIL_PORT=465
+    MAIL_USE_SSL=True
+    MAIL_USERNAME = 'sentijournalapp@gmail.com'
+    MAIL_PASSWORD = 'sentijournalapp'
+
+
+
+
